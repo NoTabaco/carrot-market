ffc659854a5657f371575299295cbec4f627fa4a
NoTabaco <adslonese@gmail.com>
2023-02-19 01:53:56 +0900

    Test Drive part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
 export default function Home() {
   return (
-    <div className="bg-red-500 text-center flex">
-      <h1 className="text-black">it works</h1>
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="bg-white p-7 rounded-3xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="mt-2 pt-2 border-t-2 border-dashed flex justify-between">
+          <span>Total</span>
+          <span className="font-semibold">$38</span>
+        </div>
+        <div className="mt-4 w-1/2 mx-auto bg-blue-500 text-white text-center p-3 rounded-xl">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-3xl shadow-xl"></div>
     </div>
   );
 }
