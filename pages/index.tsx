9b3cba3f5504ce477150b3102408140ec6368559
NoTabaco <adslonese@gmail.com>
2023-04-12 00:28:56 +0900

    More Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
 export default function Home() {
   return (
-    <form className="flex flex-col space-y-2 p-5">
-      <input
-        type="text"
-        required
-        placeholder="Username"
-        className="border p-1 peer border-gray-400 rounded-md"
-      />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">
-        Awesome Username
-      </span>
-      <span className="hidden peer-hover:block peer-hover:text-amber-500">
-        Hello
-      </span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+    <div className="flex flex-col space-y-2 p-5">
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400">
+        Hello everyone!
+      </p>
+    </div>
   );
 }
