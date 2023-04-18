d486b09816749e1a18b35e798619ddaf8744724e
NoTabaco <adslonese@gmail.com>
2023-04-19 00:20:44 +0900

    Responsive Modifiers part Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
-      <div className="bg-white sm:hover:bg-pink-800 sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500 p-7 rounded-3xl shadow-xl">
+    <div className="bg-slate-400 xl:place-content-center py-20 px-10 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <div className="bg-white flex flex-col justify-between p-7 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <ul>
           {[1, 2].map((_, index) => (
@@ -19,30 +19,30 @@ export default function Home() {
           Checkout
         </button>
       </div>
-      <div className="bg-blue-500 rounded-3xl shadow-xl">
-        <div className="p-6 pb-16 flex justify-between">
+      <div className="portrait:bg-indigo-600 landscape:bg-teal-500 rounded-3xl shadow-xl group">
+        <div className="p-6 pb-16 lg:pb-[50px] flex justify-between xl:pb-[118px]">
           <span className="text-white text-xl">Profile</span>
           <span className="text-white text-xl">🇺🇸</span>
         </div>
-        <div className="rounded-[32px] p-6 bg-white relative -top-5 h-[70%]">
+        <div className="rounded-[32px] lg:rounded-3xl p-6 bg-white relative -top-5 h-[70%]">
           <div className="flex justify-between items-end relative -top-16">
             <div className="flex flex-col items-center relative top-4 left-3">
               <span className="mb-[5px] text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-gray-300 rounded-full" />
+            <div className="h-24 w-24 bg-zinc-300 rounded-full group-hover:bg-red-300 transition-colors" />
             <div className="flex flex-col items-center relative top-4 right-3">
               <span className="mb-[5px] text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,310</span>
             </div>
           </div>
-          <div className="flex flex-col items-center -mt-7 -mb-10">
+          <div className="flex flex-col items-center -mt-7 -mb-12">
             <span className="font-medium text-lg">Tony Molloy</span>
             <span className="text-sm text-gray-500">New York, USA</span>
           </div>
         </div>
       </div>
-      <div className="bg-white p-7 rounded-3xl shadow-xl mt-[22px]">
+      <div className="bg-white p-7 rounded-3xl shadow-xl mt-[26px] lg:mt-0 lg:col-span-2 xl:col-span-1">
         <div className="flex justify-between items-center mb-5">
           <span>⬅️</span>
           <div className="space-x-4">
