f7891ffd10d493bc7de6c898b6f76dcff7878f07
NoTabaco <adslonese@gmail.com>
2023-05-06 00:53:47 +0900

    Auth part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="dark:md:hover:bg-teal-400 bg-[url('/vercel.svg')]">
-      <h2 className="text-[97851px] text-[#000]">Hello</h2>
-    </div>
-  );
+  return null;
 }
